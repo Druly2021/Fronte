e4e8ea1da1508c36b0e761ee3663e4a9dfc99bf6
Andrey Shchepetov <shchepetov2012@yandex.ru>
2024-04-09 07:25:53 +0300

    Переименовал

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def index():
 
 @app.route("/logo/<string:name>/<string:surname>/<int:age>")
 def person(name, surname, age):
-    return render_template("index.html", name=name, surname=surname,
+    return render_template("logo.html", name=name, surname=surname,
                            age=age, title="Страница знакомств")
 
 
