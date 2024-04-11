7797a32ceb0fa421486a98387033c4553262562e
Andrey Shchepetov <shchepetov2012@yandex.ru>
2024-04-11 12:14:22 +0300

    Подправил код

--- main_1.py
+++ main_1.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/numbers")
 def numbers():
-    return render_template("numbers.html", title="Числа от 1 до 50, кратные 3.")
+    return render_template("numbers.html", title="Числа от 1 до 50")
 
 
 if __name__ == "__main__":
